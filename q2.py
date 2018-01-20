229b221a8b957094025ae325756b9b2fcf53205c
adixsyukri <adi.syukri.yusof@gmail.com>
2018-01-20 23:34:57 +0800

    convert all codes to use pandas

--- q2.py
+++ q2.py
@@ -1,62 +1,64 @@
 import requests
 import numpy as np
+import pandas as pd
 import yaml
 from kaggle import Kaggle
 
 # Download dataset process
 DATA_URL = "https://www.kaggle.com/crawford/weekly-sales-transactions/downloads/Sales_Transactions_Dataset_Weekly.csv"
+SETTINGS = load_settings()
 
 def load_settings():
     with open('config.yaml', 'r') as sf:
         settings = yaml.load(sf.read())
     return settings
 
+def chunk(df, n):
+    for i in range(0, df.shape[0], n):
+        yield df[i:i + n]
 
-def median_mean_stdev(data):
-    sorted_array = sorted(np.array(data).astype(np.float))
-    median_data = np.median(sorted_array)
-    print('median_data', median_data)
-    mean_data = np.mean(sorted_array)
-    print('mean_data', mean_data)
-    stdev_data = np.std(sorted_array)
-    print('stdev_data', stdev_data)
-    threshold = 3
+def cleanup(arr):
+    '''Cleanup Kaggle dataset since there are additional columns'''
+    index = 0
+    for i, x in enumerate(arr[0]):
+        if (x.find('Normalized') == 0):
+            continue
+        if (x.find('MAX') == 0):
+            continue
+        if (x.find('MIN') == 0):
+            continue
+        index = i
+    
+    return [x[:index] for x in arr if x]
 
-    z_scores = [(x - mean_data) / stdev_data for x in sorted_array]
-    outlier_indexes, = np.where(np.abs(z_scores) > threshold)
-    print('outlier_indexes', outlier_indexes)
-    return (median_data, mean_data, stdev_data, outlier_indexes)
-
-def chunk(arr, n):
-    for i in range(0, len(arr), n):
-        yield sum(arr[i:i + n])
-
-
-kaggle = Kaggle(DATA_URL)
-kaggle.login(load_settings())
+kaggle = Kaggle(DATA_URL, SETTINGS)
 kaggle_data = kaggle.get_data()
-header = kaggle_data[0]
-dataset = kaggle_data[1:]
+processed_data = cleanup(kaggle_data)
+np_transpose_data = np.transpose(np.array(processed_data)) #transpose data so that product become the columns
+fullData = pd.DataFrame(data=np_transpose_data[1:, 1:].astype(np.float), index=np_transpose_data[1:,0], columns=np_transpose_data[0,1:])
 
+# Median for all product
+median_data = fullData.median()
 
-# Search for median
-final = []
-for index, row in enumerate(dataset):
-    if len(row) == 1:
-        continue
-    product = row[0]
-    data = [float(x) for x in row[1:] if x]
-    median_data, mean_data, stdev_data, outlier_indexes = median_mean_stdev(data)
-    outliers = [(header[x+1], data[x]) for x in outlier_indexes if x]
-    final.append({
-        'product': row[0],
-        'median': median_data,
-        'mean': mean_data,
-        'min': min(data),
-        'max': max(data),
-        'total': sum(data),
-        'biweekly': [x for x in chunk(data, 2) if x],
-        'outliers': outliers
-    })
+# Mean for all product
+mean_data = fullData.mean()
 
-print final
+# Min for all product
+min_data = fullData.min()   
+
+# Max for all product
+max_data = fullData.max()
+
+# Best product based on volume
+sum_of_product = fullData.sum()
+index_array, = np.where(sum_of_product == sum_of_product.max())
+best_product = (fullData.columns[index_array[0]], sum_of_product.max())
+
+# Group biweekly sales
+grouped = chunk(fullData, 2)
+sum_grouped = [i.sum() for i in grouped]
+worst_5_product_biweekly = [('Biweekly {}'.format(i+1), x.nsmallest(5)), for i, x in enumerate(sum_grouped)]
+
+# Get outliers for every product
+outliers = fullData[~(np.abs(fullData - fullData.mean())<=(3*fullData.std()))]
+
